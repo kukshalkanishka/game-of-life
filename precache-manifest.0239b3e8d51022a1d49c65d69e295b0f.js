a7d538fb9a5bc99e45976e9b44ceb812c30accc5
Kanishka kuksal <37463477+kukshalkanishka@users.noreply.github.com>
2019-04-02 12:29:21 +0530

    Updates

---
+++ precache-manifest.0239b3e8d51022a1d49c65d69e295b0f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2f875ddf8d49c7e02dce",
+    "url": "/game-of-life/static/css/main.2296a880.chunk.css"
+  },
+  {
+    "revision": "2f875ddf8d49c7e02dce",
+    "url": "/game-of-life/static/js/main.3851d70f.chunk.js"
+  },
+  {
+    "revision": "b9d05e43578a5ddb80d1",
+    "url": "/game-of-life/static/js/runtime~main.dfaa7c2d.js"
+  },
+  {
+    "revision": "53405d50c4f510bb36eb",
+    "url": "/game-of-life/static/js/2.6d256598.chunk.js"
+  },
+  {
+    "revision": "bdb9e779203d1ba301d9641425e30725",
+    "url": "/game-of-life/index.html"
+  }
+];
